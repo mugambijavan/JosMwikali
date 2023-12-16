f8fd2006fa1024ebcca733df8dbede7e00350dbf
Mugambi Javan <mugambijavan@gmail.com>
2023-12-16 23:49:47 +0300

    data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
           Jos Mwikali
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Data Engineer.
+          I'm a Data Analyst.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a passionate and results-driven data engineer with a keen interest in transforming raw data into valuable insights. My journey in the world of data engineering has been shaped by a commitment to excellence, a curiosity for problem-solving, and a love for leveraging cutting-edge technologies. As a dedicated professional, I thrive on the challenges of designing robust data architectures and optimizing data pipelines to unlock the full potential of information
+          I’m a passionate and results-driven Data Analyst with a keen interest in transforming raw data into valuable insights. My journey in the world of data engineering has been shaped by a commitment to excellence, a curiosity for problem-solving, and a love for leveraging cutting-edge technologies. As a dedicated professional, I thrive on the challenges of designing robust data architectures and optimizing data pipelines to unlock the full potential of information
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
